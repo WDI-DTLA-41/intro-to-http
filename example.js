0ed6f57cb878dbb33bc95b4f8adb4d0a56d05734
Phil Lamplugh <phillip.lamplugh@gmail.com>
2017-01-05 12:50:15 -0800

    add solution

--- example.js
+++ example.js
@@ -1,14 +1,26 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 3000;
 
+// if a CLIENT requests '/' send back the index.html file's contents
+// otherwise send back 404 page not found
 var server = http.createServer(function(request, response) {
-  console.log('Incoming ' + request.method + 'request!');
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.write('Hello\n');
-  response.end();
+  console.log('Incoming ' + request.method + 'request' + request.url);
+  response.setHeader('Content-Type', 'text/html');
+  if (request.url === '/') {
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+      if (err) throw err;
+      response.statusCode = 200;
+      response.write(data)
+      response.end();
+    })
+  } else {
+    response.statusCode = 404;
+    response.write('Not Found!');
+    response.end();
+  }
 });
 
 server.listen(port, hostname, function(){
